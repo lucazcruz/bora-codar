060335690971ea0b4cf1370d39dc2393d6cc9b76
lucazcruz <lucaz.cruz17@gmail.com>
2023-05-23 15:26:05 -0300

    percentage added and somes improvement

--- calculator/script.js
+++ calculator/script.js
@@ -1,27 +1,23 @@
 const calculo = document.querySelector(".calculo");
 const result = document.querySelector(".result");
 
-function invalid() {
-  result.innerText = "invalid!";
-  const interval = setTimeout(() => result.innerText= "0", 1500)
-}
-
-function display(n) {
-  if (result.innerText == "0" && !isNaN(n)) {
-    result.innerText = "";
+function sizeHandle(length) {
+  if (length < 19) {  
+    result.style.fontSize= `22px`
+    if (length <= 15) {
+      result.style.fontSize= `28px`
+      if (length <= 11) {
+        result.style.fontSize= `36px`
+      }
+    }
+    return;
   }
-  if (result.innerText.length > 9) {
-    result.innerText = result.innerText.slice(1);
-  }
-
-  const last = isNaN(result.innerText.slice(-1));
-  if (last && isNaN(n)) {
-    backspace();
-  }
-  result.innerText += n;
+  result.style.fontSize= `22px`
+  result.innerText = result.innerText.slice(0, 19);
 }
 
 function backspace() {
+  sizeHandle(result.innerText.length)
   result.innerText = result.innerText.slice(0, -1);
   if (result.innerText == "") result.innerText= "0";
 }
@@ -29,13 +25,44 @@ function backspace() {
 function clearDisplay() {
   calculo.innerText = "";
   result.innerText = "0";
+  sizeHandle(result.innerText.length)
+}
+
+function invalid() {
+  result.innerText = "invalid!";
+  const interval = setTimeout(() => result.innerText= "0", 1500)
+}
+
+function validation(n) {
+  if (result.innerText == "0" && !isNaN(n)) {
+    result.innerText = "";
+  }
+  const last = isNaN(result.innerText.slice(-1));
+  if (last && isNaN(n)) {
+    backspace();
+  }
+}
+
+function display(n) {
+  validation(n);
+  result.innerText += n;
+  sizeHandle(result.innerText.length);
+}
+
+function percentage() {
+  const arrNum = result.innerText.split(/[+\*\-\/\%]/)
+  const lastNum = arrNum[arrNum.length-1];
+  result.innerText = result.innerText.replace(`${lastNum}`, lastNum/100);
+  sizeHandle(result.innerText.length);
 }
 
 function calculate() {
   try {
     if (eval(result.innerText) === undefined) throw new Error();
     calculo.innerText = result.innerText;
-    result.innerText = String(eval(result.innerText)).slice(0,9);
+    result.innerText = eval(result.innerText);
+    sizeHandle(result.innerText.length);
+
   } catch(error) {
     invalid();
   }
