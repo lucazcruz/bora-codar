41b348a2e5f7275f50d7a44728e34e3b6bdf4d52
lucazcruz <lucaz.cruz17@gmail.com>
2023-06-10 18:41:33 -0300

    novas funcionalidades

--- credit-card-form/js/ToggleCard.js
+++ credit-card-form/js/ToggleCard.js
@@ -3,19 +3,28 @@ export default class ToggleCard {
     this.form = document.forms[0];
     this.creditCard = document.querySelector(".credit-card");
 
-    this.cardToggle = this.cardToggle.bind(this);
+    this.cardToggleShow = this.cardToggleShow.bind(this);
+    this.showFront = this.showFront.bind(this);
   }
 
-  cardToggle({ type }) {
+  cardToggleShow({ type }) {
     if (type === "click") this.creditCard.classList.toggle("back");
     if (type === "focus") this.creditCard.classList.add("back");
     if (type === "focusout") this.creditCard.classList.remove("back");
   }
 
+  showFront() {
+    this.creditCard.classList.remove("back");
+  }
+
   init() {
-    this.creditCard.addEventListener("click", this.cardToggle);
-    this.form.cvv.addEventListener("focus", this.cardToggle);
-    this.form.cvv.addEventListener("focusout", this.cardToggle);
+    this.creditCard.addEventListener("click", this.cardToggleShow);
+    this.form.cvv.addEventListener("focus", this.cardToggleShow);
+    this.form.cvv.addEventListener("focusout", this.cardToggleShow);
+
+    this.form.inputNumber.addEventListener("focus", this.showFront);
+    this.form.inputNumber.addEventListener("focus", this.showFront);
+    this.form.validity.addEventListener("focus", this.showFront);
   }
 }
 
