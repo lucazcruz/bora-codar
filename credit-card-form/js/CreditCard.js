ab28e8948de201c006795551f68214ba2a810c65
lucazcruz <lucaz.cruz17@gmail.com>
2023-06-12 12:31:23 -0300

    checkCardBand added

--- credit-card-form/js/CreditCard.js
+++ credit-card-form/js/CreditCard.js
@@ -5,12 +5,30 @@ export default class CreditCard {
     this.cardName = document.querySelector("#card-name");
     this.cardValidity = document.querySelector("#card-validity");
     this.cardCvv = document.querySelectorAll("#card-cvv span");
+    this.cardBand = document.querySelector("#card-band")
 
-    this.keyUp = this.keyUp.bind(this);
+    this.input = this.input.bind(this);
     this.formError = this.formError.bind(this);
   }
 
-  replaceNumbersInCard(arrayCard, inputValues = []) {
+  checkCardBand(target ) {           // Checa a bandeira do cartão;
+    const cleanValue = target.value.replace(/\s/g, '');
+
+    var visaRegex = /^4[0-9]{12}(?:[0-9]{3})?$/;
+    var mastercardRegex = /^(5[1-5][0-9]{14})$/;
+    var eloRegex = /^(401178|401179|431274|438935|451416|457393|457631|457632|504175|506699|5067|509\\d|627780|636297|636368|636369|655000|655021|655056|6504|6504|6505|6506|506699|50670[0-9]|50671[0-9]|50672[0-9]|50673[0-9]|50674[0-9]|50675[0-9]|50676[0-9]|50677[0-9]|50678[0-9]|50679[0-9]|5067[0-9][0-9]|627780|636297|636368|636369|6504|6504|6505|6506|6510)[0-9]{10,13}$/;
+
+    this.cardBand.innerHTML = '';
+    if (eloRegex.test(cleanValue)) {
+      this.cardBand.innerHTML = `<img src="./assets/elo.svg" alt="card elo">`
+    } else if (mastercardRegex.test(cleanValue)) {
+      this.cardBand.innerHTML = `<img src="./assets/mastercard.svg" alt="card mastercard">`
+    } else if (visaRegex.test(cleanValue)) {
+      this.cardBand.innerHTML = `<img src="./assets/visa.svg" alt="card visa">`
+    }
+  }
+
+  replaceNumbersInCard(arrayCard, inputValues = []) {       // ADICIONA INFO NUMÉRICAS AO CARTÃO;
     arrayCard.forEach((span, i) => {
       if(inputValues[i]) {
         span.innerHTML = inputValues[i]
@@ -24,7 +42,7 @@ export default class CreditCard {
     })
   }
 
-  characterValidity(target) {  // SUBSTITUI CHARACTER INVÁLIDO
+  characterValidity(target) {            // REMOVE CHARACTER INVÁLIDO;
     const onlyNumber = /[^0-9]/g;            // ACEITA APENAS NUMEROS DE 0-9
     const onlyLetters = /[^a-zA-Z\s]/g;      // ACEITA APENAS LETRAS E ESPAÇAMENTO
 
@@ -37,25 +55,26 @@ export default class CreditCard {
     if (target === this.form.validity) target.value = target.value.replace(/(\d{2})(?=\d)/g, '$&/');
   }
 
-  handleInputNumber(target) {
+  handleInputNumber(target) {          // FORMATA E CHAMA A FUNÇÃO DE ADICIONAR OS NUMEROS AO CARTÃO;
     this.characterValidity(target);
     const value = target.value.replace(/\s/g, ''); // REMOVE OS ESPAÇOS EM BRANCO
     this.replaceNumbersInCard(this.cardNumber, [...value]); // ADICIONA NUMEROS AO CARTÃO
     target.value = value.replace(/(\d{4})(?=\d)/g, '$& '); // ESPAÇO A CADA 4 DIGITOS
+    this.checkCardBand(target);
   }
 
-  handleTutelaryName(target) {
+  handleTutelaryName(target) {        // ADICIONA NOME DO TITULAR AO CARTÃO;
     this.characterValidity(target);
     this.cardName.innerText = target.value = target.value;
     if (this.cardName.innerText === '') this.cardName.innerText = 'Seu nome aqui';
   }
 
-  handleInputValidity(target) {
+  handleInputValidity(target) {     // ADICIONA A DATA DE VALIDADE AO CARTÃO;
     this.characterValidity(target);
     this.cardValidity.innerText = target.value;
   }
 
-  formError({ target }) {
+  formError({ target }) {          // VALIDA A DATA E NUMERO DO CARTÃO;
     target.nextElementSibling.innerText = "";
     target.classList.remove("invalid");
 
@@ -100,21 +119,21 @@ export default class CreditCard {
     }
   }
 
-  handleCvv(target) {
+  handleCvv(target) {            // ADICIONA CVV AO CARTÃO;
     this.characterValidity(target);
     this.replaceNumbersInCard(this.cardCvv, [...target.value]);
   }
 
-  keyUp({ target }) {
+  input({ target }) {          // FILTRA OS ALVOS;
     if (target === this.form.inputNumber) this.handleInputNumber(target);
     if (target === this.form.tutelary) this.handleTutelaryName(target);
     if (target === this.form.validity) this.handleInputValidity(target);
     if (target === this.form.cvv) this.handleCvv(target);
   }
 
-  init() {
-    this.form.addEventListener("input", this.keyUp);
+  init() {                   // INICIA A CLASS;
+    this.form.addEventListener("input", this.input);
     this.form.addEventListener("change", this.formError);
     this.replaceNumbersInCard(this.cardNumber);
-  }
+  } 
 }
