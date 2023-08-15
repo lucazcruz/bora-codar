3b74dc86e1f42dca3e535c876866ccf6bfd2ddbc
lucazcruz <lucaz.cruz17@gmail.com>
2023-08-15 20:55:50 -0300

    added new features at script

--- shopping-cart/script.js
+++ shopping-cart/script.js
@@ -1,6 +1,27 @@
 class Cart {
   constructor() {
     this.amountElements = document.querySelectorAll(".amount");
+    this.cardsElements = document.querySelectorAll(".itens .card");
+    this.totalElement = document.querySelector(".amount-total");
+  }
+
+  formatValue(item) {
+    const value = item.querySelector("h3").innerText;
+    const valueItem = value.replace("R$ ", "").replace(".", "").replace(",", ".");
+    return +valueItem;
+  }
+
+  attTotal() {
+    let total = 0
+    this.cardsElements.forEach(card => {
+      const cardValue = this.formatValue(card);
+      const amount = card.querySelector(".amount span").innerText;
+
+      total += cardValue * amount
+    })
+
+    const totalBRL = total.toLocaleString("pt-BR", {style:"currency", currency:"BRL"});
+    this.totalElement.innerText = totalBRL;
   }
 
   amountObject(amount) {
@@ -17,27 +38,28 @@ class Cart {
     if (amountObj.amountElement.innerText < 2) {
       amountObj.minusButton.setAttribute("disabled", '');
       amountObj.amountElement.innerText = 1;
-      return true
+      return
     }
 
     amountObj.minusButton.removeAttribute("disabled");
-    return false;
   }
 
   moreItem(amountObj) {
     const amount =  +amountObj.amountElement.innerText;
     amountObj.amountElement.innerText = amount +1;
     this.amountVerification(amountObj);
+    this.attTotal();
   }
 
   minusItem(amountObj) {
     const amount =  +amountObj.amountElement.innerText;
     amountObj.amountElement.innerText = amount -1;
     this.amountVerification(amountObj);
+    this.attTotal();
   }
 
-  cartEvents({ target }, amount) {
-    const amountObj = this.amountObject(amount)
+  cartEvents({ target }, amountElement) {
+    const amountObj = this.amountObject(amountElement)
     if (amountObj.plusButton === target || target.parentElement === amountObj.plusButton) this.moreItem(amountObj);
     if (amountObj.minusButton === target || target.parentElement === amountObj.minusButton) this.minusItem(amountObj);
   }
