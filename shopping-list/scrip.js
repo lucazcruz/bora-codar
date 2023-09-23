d9731d702f3ec2f3b099cd0253349be409d30b16
lucazcruz <lucaz.cruz17@gmail.com>
2023-09-23 08:50:13 -0300

    some functions removed

--- shopping-list/scrip.js
+++ shopping-list/scrip.js
@@ -1,10 +1,11 @@
 class ShoppingList {
   constructor() {
+    this.body = document.body;
+    this.main = document.querySelector("main");
     this.form = document.forms[0];
+
     this.selects = this.form.querySelectorAll(".select")
     this.unitiesSelected = this.form.querySelector("#unities-select .selected-value")
-    this.main = document.querySelector("main");
-    this.body = document.body;
 
     this.addNewItem = this.addNewItem.bind(this);
     this.changeOptions = this.changeOptions.bind(this);
@@ -41,21 +42,9 @@ class ShoppingList {
         <span data-category="${formData.category.toLowerCase()}" >
           ${formData.category}
         </span>
-        <div class="buttons">
-            <div class="moreButton">
-              <i data-lucide="more-vertical"></i>
-              <input type="checkbox">
-            </div>
-
-            <div class="modal">
-              <button class="editButton">
-                <i data-lucide="pencil"></i>
-              </button>
-              <button class="deleteButton">
-                <i data-lucide="trash-2"></i>
-              </button>
-            </div>
-          </div>
+        <div class="right">
+          <i data-lucide="more-vertical"></i>
+        </div>
       </div>
     `
     return item;
@@ -88,53 +77,12 @@ class ShoppingList {
     this.saveData(data);
   }
 
-  deleteItem(index) {
-    const data = this.getData();
-    data.splice(index, 1);
-    this.saveData(data);
-    this.loadData();
-  }
-
-  saveEdit(e, index) {
-    e.preventDefault();
-    const formPrototype = new FormData(this.form);
-    const formData = Object.fromEntries(formPrototype);
-
-    const data = this.getData()
-    data[index] = formData;
-    this.saveData(data);
-    this.loadData();
-
-    this.form.addEventListener("submit", this.addNewItem);
-  }
-
-  editItem(index) {
-    const data = this.getData();
-    const types = {
-      "UN.": "Unidade",
-      "L": "Litro",
-      "kg": "Kilograma"
-    }
-    
-    const unitType = types[data[index].unitType];
-
-    this.form["item-name"].value = data[index].item;
-    this.form.unities.value = data[index].unities;
-    this.form.querySelector(`[data-label='${unitType}']`).click()
-    this.form.querySelector(`[data-label='${data[index].category}']`).click()
-
-    this.form.removeEventListener("submit", this.addNewItem);
-    this.form.onsubmit = (e) => this.saveEdit(e, index);
-  }
-
   loadData() {
     this.main.innerHTML= "";
     const data = this.getData();
     data.forEach((element, index) => {
       const item = this.createItem(element);
       item.addEventListener("change", (e) => this.isChecked(e, index))
-      item.querySelector(".deleteButton").addEventListener("click", () => this.deleteItem(index));
-      item.querySelector(".editButton").addEventListener("click", () => this.editItem(index));
       this.main.appendChild(item)
     });
 
@@ -179,6 +127,5 @@ class ShoppingList {
   }
 }
 
-  const shoppingList = new ShoppingList();
-  shoppingList.init();
-  
+const shoppingList = new ShoppingList();
+shoppingList.init();
