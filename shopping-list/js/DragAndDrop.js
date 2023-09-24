2ceb7500968aae24256a90928e35e122dd819a20
lucazcruz <lucaz.cruz17@gmail.com>
2023-09-23 22:48:56 -0300

    DragAndDrop working in mobile devices

--- shopping-list/js/DragAndDrop.js
+++ shopping-list/js/DragAndDrop.js
@@ -16,11 +16,11 @@ export default class DragAndDrop {
   getNewPosition(posY) {
     const items = this.main.querySelectorAll(".item:not(.dragging)");
     let result;
-
+    
     items.forEach(item => {
       const box = item.getBoundingClientRect();
       const cardCenterY = box.y + box.height / 2;
-    
+
       if (posY >= cardCenterY) result = item;
     })
 
@@ -28,8 +28,12 @@ export default class DragAndDrop {
   }
 
   onDragover(e) {
+    e.preventDefault();
+
+    const posY = e.type === "dragover" ? e.clientY : e.changedTouches[0].clientY;
+
     const dragging = this.main.querySelector(".dragging");
-    const applyAfter = this.getNewPosition(e.clientY);
+    const applyAfter = this.getNewPosition(posY);
 
     if (applyAfter) {
       applyAfter.insertAdjacentElement("afterend", dragging);
@@ -42,6 +46,10 @@ export default class DragAndDrop {
     document.addEventListener("dragstart", this.onDragstart);
     document.addEventListener("dragend", this.onDragend);
     this.main.addEventListener("dragover", this.onDragover);
+
+    document.addEventListener("touchstart", this.onDragstart);
+    document.addEventListener("touchend", this.onDragend);
+    this.main.addEventListener("touchmove", this.onDragover);
   }
 
   init() {
